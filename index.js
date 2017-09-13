a915921d949ec252dbae83b084df23dc6e57e7f8
Unknown <mj0857363@otc.edu>
2017-09-13 10:04:53 -0500

    Comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//require() - loads and caches modules
 const M = require('moment')
 const _ = require('lodash')
 const chalk = require('chalk')
 const MomentRange = require('moment-range')
 
+//Create an object?
 const moment = MomentRange.extendMoment(M)
 
 let year = moment().range('year');
