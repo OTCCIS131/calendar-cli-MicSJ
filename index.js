78f208afc45f70db0016e36b8953df3c1cc93251
Unknown <mj0857363@otc.edu>
2017-09-13 08:04:29 -0500

    Headings

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let year = moment().range('year');
 _.forEach(Array.from(year.by('month')), month =>{   
     console.log(_.pad(month.format('MMMM'), 26, ' '))
-    console.log('')
+    console.log('S  M  T  W  Th  F  S  ')
+
+    let monthRange = month.range('month')
+    let firstDay = monthRange.start.day()
+
+    console.log(firstDay)
 })
 
 
