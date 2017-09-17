bb1cfd05357fe71703724b457b9dfc3997c7d480
Unknown <mj0857363@otc.edu>
2017-09-17 15:18:10 -0500

    Chunking added

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-//require() - loads and caches modules
-const M = require('moment')
+const Moment = require('moment')
 const _ = require('lodash')
 const chalk = require('chalk')
 const MomentRange = require('moment-range')
 
-const moment = MomentRange.extendMoment(M)
+const moment = MomentRange.extendMoment(Moment)
 
-let year = moment().range('year');
-_.forEach(Array.from(year.by('month')), month =>{ 
-    //parse and format  
-    console.log(_.pad(month.format('MMMM'), 26, ' '))
-    console.log('S  M  T  W  Th  F  S  ')
+const now = moment()
+let year = now.range('year')
 
-    let monthRange = month.range('month')
-    let firstDay = monthRange.start.day()
+for(const month of year.by('month'))
+    {
+        console.log(_.pad(month.format('MMMM'), 26, ' '))//grid 26 characters wide
+        console.log('S   M   T   W   Th  F   S ')
 
-    console.log(firstDay)
-})
+        let days = Array.from(month.range('month').by('days'))
+
+        let paddedDays = _.map(days, day => {
+           let date = day.date()
+           if (day.month() == 9 && day.date() == 10){
+               date = chalk.cyan(date)
+           }
+           if (day.month() == 12 && day.date() == 23){
+               date = chalk.cyan(date)
+           }
+
+           return _.padEnd(date, 2, ' ')
+        })
+
+        let week = _.chunk(Array.from(paddedDays), 7)
 
 
 
+    console.log(week)
+    }
