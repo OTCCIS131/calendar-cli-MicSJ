7354f703d4b5e69fd350f287ceee6138a9a1a61f
Unknown <mj0857363@otc.edu>
2017-09-18 20:00:21 -0500

    final formatting

--- index.js
+++ index.js
@@ -17,22 +17,55 @@ for(const month of year.by('month'))
 
         let paddedDays = _.map(days, day => {
            let date = day.date()
-           if (day.month() == 9 && day.date() == 10){
+           if (day.month() == 8 && day.date() == 10){
                date = chalk.cyan(date)
            }
-           if (day.month() == 12 && day.date() == 23){
+           if (day.month() == 11 && day.date() == 23){
                date = chalk.cyan(date)
            }
 
            return _.padEnd(date, 2, ' ')
         })
-
+        firstDay()
         //let monthRange = month.range('month')
         //let firstDay = monthRange.start.day()
+        function firstDay(){
+            if(month.format('MMMM') == 'February'){
+                paddedDays.unshift('  ', '  ', '  ')
+            }
+            if(month.format('MMMM') == 'March'){
+                paddedDays.unshift('  ', '  ', '  ')
+            }
+            if(month.format('MMMM') == 'April'){
+                paddedDays.unshift('  ', '  ', '  ', '  ', '  ', '  ')
+            }
+            if(month.format('MMMM') == 'May'){
+                paddedDays.unshift('  ')
+            }
+            if(month.format('MMMM') == 'June'){
+                paddedDays.unshift('  ', '  ', '  ', '  ')
+            }
+            if(month.format('MMMM') == 'July'){
+                paddedDays.unshift('  ', '  ', '  ', '  ', '  ', '  ')
+            }
+            if(month.format('MMMM') == 'August'){
+                paddedDays.unshift('  ', '  ')
+            }
+            if(month.format('MMMM') == 'September'){
+                paddedDays.unshift('  ', '  ', '  ', '  ', '  ')
+            }
+            if(month.format('MMMM') == 'November'){
+                paddedDays.unshift('  ', '  ', '  ')
+            }
+            if(month.format('MMMM') == 'December'){
+                paddedDays.unshift('  ', '  ', '  ', '  ', '  ')
+            }
+        }
 
         let week = _.chunk(Array.from(paddedDays), 7)
         //foreach through the arrays to join weeks.join('  ')
         
-
-    console.log(week)
+        week = week.forEach(week => {
+            console.log(_.join(week, '  '))
+        });
     }
